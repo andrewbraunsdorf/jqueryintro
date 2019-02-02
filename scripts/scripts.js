55caa3b0b7d6faa06eb6db11bf87cab045e0f9ed
Andrew Braunsdorf <andrewbraunsdorf.student@careerdevs.com>
2019-02-02 16:54:31 +0000

    added jQuery statements

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,3 +1,7 @@
- $(document).ready(function(){
- 	alert("jquery loaded");
- })
+ // $("#lead-banner p");
+ 
+ $("h3").css({border: "3px solid blue"});
+ 
+ $(".wrapper").css({border: "3px solid red"});
+ 
+ $("#clients").css({border: "3px solid yellow"});
