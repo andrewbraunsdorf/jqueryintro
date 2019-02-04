fea5b696c4aaed9a8a3956f3e1dc6c754e20ef0a
Andrew Braunsdorf <andrewbraunsdorf.student@careerdevs.com>
2019-02-04 23:05:19 +0000

    used jquery filters

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,7 +1,17 @@
- // $("#lead-banner p");
+// $("header nav li:first").css({border: "2px solid red"});
+// $("header nav li:last").css({border: "2px solid red"});
+
+// $("#contact ul:first").css({border: "2px solid red"});
+// $("#contact ul:last").css({border: "2px solid red"});
+
+
+$("header nav li:even").css({border: "2px solid blue"});
+$("header nav li:odd").css({border: "2px solid yellow"});
+
+ // // $("#lead-banner p");
  
- $("h3").css({border: "3px solid blue"});
+ // $("h3").css({border: "3px solid blue"});
  
- $(".wrapper").css({border: "3px solid red"});
+ // $(".wrapper").css({border: "3px solid red"});
  
- $("#clients").css({border: "3px solid yellow"});
+ // $("#clients").css({border: "3px solid yellow"});
