08ae7538e2d9385fdc10c2decf191b72516ebc71
Andrew Braunsdorf <andrewbraunsdorf.student@careerdevs.com>
2019-02-05 00:50:57 +0000

    added filters to show attributes

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -5,8 +5,26 @@
 // $("#contact ul:last").css({border: "2px solid red"});
 
 
-$("header nav li:even").css({border: "2px solid blue"});
-$("header nav li:odd").css({border: "2px solid yellow"});
+// $("header nav li:even").css({border: "2px solid blue"});
+// $("header nav li:odd").css({border: "2px solid yellow"});
+
+// $("section:not('#contact')").css({border: "2px solid green"});
+
+// //less than
+// $("#social-nav li:lt(3)").css({border: "2px solid blue"});
+// //greater than
+// $("#social-nav li:gt(2)").css({border: "2px solid purple"});
+
+//attribute filters
+// $("div[class]").css({border: "2px solid blue"});
+
+// $("img[alt]").css({border: "2px solid pink"});
+
+//attribute filters
+$("img[alt=quote]").css({border: "2px solid purple"});
+
+
+
 
  // // $("#lead-banner p");
  
