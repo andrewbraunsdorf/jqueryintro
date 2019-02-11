7e511a7f0eeb461ae1c3361ded96b3fd78e2b9b7
Andrew Braunsdorf <andrewbraunsdorf.student@careerdevs.com>
2019-02-11 16:59:54 +0000

    used adding and changing content

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,3 +1,15 @@
-$("#contact-methods").css({border: "2px solid red"})
-. next().css({border: "2px solid green"})
-.closest("section").css({border: "2px solid blue"});
+var tweet = "<div style='margin: 20px 0; padding: 10px; background: #eee'>The big fight live: Ham vs Cheese!</div>"
+
+$("#tweets p").text(tweet)
+// //ADDING & Changing Content
+
+// .append() adds content to bottom of the element
+// .prepend() adds content to the top of element
+// .before() adds content before element
+// .after() adds content after element
+// .html() changeds the whole html of element
+// .text() changes text of an element
+
+// $("#contact-methods").css({border: "2px solid red"})
+// . next().css({border: "2px solid green"})
+// .closest("section").css({border: "2px solid blue"});
